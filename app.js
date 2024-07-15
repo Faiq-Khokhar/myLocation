92589247cb1e4639f9b5d8913455f92ab2ddb317
Faiq-Khokhar <faiq.khokhar06@gmail.com>
2024-07-15 16:20:33 +0500

    Configurations changed - FAIQ

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const app = express();
 const path = require("path");
+const app = express();
 
 const http = require("http");
 
@@ -8,8 +8,10 @@ const socketIo = require("socket.io");
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs")
+app.set('views', path.join(__dirname, 'views'));
 
 io.on("connection", (socket) => {
     socket.on("send-location", (data) => {
@@ -23,7 +25,5 @@ io.on("connection", (socket) => {
 app.get("/", (req, res) => {
     res.render("index");
 })
-
-server.listen(3000);
 
 module.exports = app;
